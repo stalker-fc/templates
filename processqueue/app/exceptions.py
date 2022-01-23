d17bbcf5e66fdb1ad47cb0f3843cc74d5a5006e9
Sergey Stepanov <stepanov.s@numdes.com>
2022-01-23 18:40:54 +0300

    update exceptions

--- processqueue/app/exceptions.py
+++ processqueue/app/exceptions.py
@@ -1,2 +1,8 @@
 class NoSuchTaskException(Exception):
-    pass
+    def __init__(self, task_id: int):
+        self.message = f"There is no task with such id = `{task_id}`."
+
+
+class NoTaskOutputDataException(Exception):
+    def __init__(self, task_id: int):
+        self.message = f"There is no output data for task with such id = `{task_id}`."
