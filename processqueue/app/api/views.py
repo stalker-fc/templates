9f64ec2f6be305ed4a737d9daf50ae14dac7f7e2
Sergey Stepanov <stepanov.s@numdes.com>
2022-02-08 18:41:16 +0300

    update views

--- processqueue/app/api/views.py
+++ processqueue/app/api/views.py
@@ -2,9 +2,17 @@ import asyncio
 
 from aiohttp import web
 
+from app.api import controller
+from app.api.model import TaskInputData
+from app.api.responses import CreateTaskResponse
 from app.api.responses import HealthCheckResponse
-from app.domain.model import TaskStatus
+from app.api.responses import IncorrectTaskOperationResponse
+from app.api.responses import NoSuchTaskResponse
+from app.api.responses import TaskInfoResponse
+from app.api.responses import TaskOutputDataResponse
+from app.api.serialization import deserialize
 from app.domain.repository import ITaskRepository
+from app.exceptions import IncorrectTaskOperationException
 from app.exceptions import NoSuchTaskException
 
 
@@ -14,153 +22,56 @@ async def healthcheck(_: web.Request) -> HealthCheckResponse:
 
 async def create_task(request: web.Request):
     data = await request.json()
-    input_data = data.get("input_data", "")
-
+    task_input_data = deserialize(TaskInputData, data)
     task_repository: ITaskRepository = request.app.get("task_repository")
-    task = await task_repository.create_task(input_data)
-
-    return web.Response(
-        status=200,
-        headers={
-            "content-type": "application/json"
-        },
-        body=str(
-            {
-                "task_id": task.task_id
-            }
-        )
-    )
+    task_info = await controller.create_task(task_repository, task_input_data)
+    return CreateTaskResponse(task_info)
 
 
 async def run_task(request: web.Request):
     data = await request.json()
     task_id = data.get("task_id", "")
-
+    task_queue: asyncio.Queue = request.app.get("task_queue")
     task_repository: ITaskRepository = request.app.get("task_repository")
-    is_task_exists = await task_repository.is_task_exists(task_id)
-    if is_task_exists:
-        task_message_queue: asyncio.Queue = request.app.get("task_message_queue")
-        await task_repository.set_task_status(task_id, TaskStatus.QUEUED)
-        await task_message_queue.put(task_id)
-
-        return web.Response(
-            status=200,
-            headers={
-                "content-type": "application/json"
-            },
-            body=str(
-                {
-                    "task_id": task_id
-                }
-            )
-        )
-    else:
-        return web.Response(
-            status=404,
-            headers={
-                "content-type": "application/json"
-            },
-            body=str(
-                {
-                    "error": "Task is not exist."
-                }
-            )
-        )
+    try:
+        task_info = await controller.run_task(task_repository, task_queue, task_id)
+        return TaskInfoResponse(task_info)
+    except NoSuchTaskException:
+        return NoSuchTaskResponse(task_id)
+    except IncorrectTaskOperationException:
+        return IncorrectTaskOperationResponse(task_id)
 
 
 async def cancel_task(request: web.Request):
     data = await request.json()
     task_id = data.get("task_id", "")
-
     task_repository: ITaskRepository = request.app.get("task_repository")
-    status = await task_repository.get_task_status(task_id)
-
-    if status is TaskStatus.QUEUED:
-        await task_repository.set_task_status(task_id, TaskStatus.CANCELLED)
-
-        return web.Response(
-            status=200,
-            headers={
-                "content-type": "application/json"
-            },
-            body=str(
-                {
-                    "task_id": task_id
-                }
-            )
-        )
-    else:
-        return web.Response(
-            status=404,
-            headers={
-                "content-type": "application/json"
-            },
-            body=str(
-                {
-                    "error": "Task is not exist."
-                }
-            )
-        )
-
+    try:
+        task_info = await controller.cancel_task(task_repository, task_id)
+        return TaskInfoResponse(task_info)
+    except NoSuchTaskException:
+        return NoSuchTaskResponse(task_id)
+    except IncorrectTaskOperationException:
+        return IncorrectTaskOperationResponse(task_id)
 
 
 async def get_task_status(request: web.Request):
-    task_id = int(request.match_info['task_id'])
+    task_id = int(request.match_info["task_id"])
+    task_repository: ITaskRepository = request.app.get("task_repository")
     try:
-        task_repository: ITaskRepository = request.app.get("task_repository")
-        status = await task_repository.get_task_status(task_id)
-        return web.Response(
-            status=200,
-            headers={
-                "content-type": "application/json"
-            },
-            body=str(
-                {
-                    "task_id": task_id,
-                    "status": status.value
-                }
-            )
-        )
+        task_info = await controller.get_task_status(task_repository, task_id)
+        return TaskInfoResponse(task_info)
     except NoSuchTaskException:
-        return web.Response(
-            status=404,
-            headers={
-                "content-type": "application/json"
-            },
-            body=str(
-                {
-                    "error": "Task is not exist."
-                }
-            )
-        )
+        return NoSuchTaskResponse(task_id)
 
 
 async def get_task_output_data(request: web.Request):
-    task_id = int(request.match_info['task_id'])
+    task_id = int(request.match_info["task_id"])
+    task_repository: ITaskRepository = request.app.get("task_repository")
     try:
-        task_repository: ITaskRepository = request.app.get("task_repository")
-        output_data = await task_repository.get_task_output_data(task_id)
-        return web.Response(
-            status=200,
-            headers={
-                "content-type": "application/json"
-            },
-            body=str(
-                {
-                    "task_id": task_id,
-                    "output_data": output_data
-                }
-            )
-        )
-    except Exception as e:
-        return web.Response(
-            status=400,
-            headers={
-                "content-type": "application/json"
-            },
-            body=str(
-                {
-                    "error": "Task output data is not exist."
-                }
-            )
-        )
+        output_data = await controller.get_task_output_data(task_repository, task_id)
+        return TaskOutputDataResponse(output_data)
+    except NoSuchTaskException:
+        return NoSuchTaskResponse(task_id)
+    except IncorrectTaskOperationException:
+        return IncorrectTaskOperationResponse(task_id)
--- processqueue/app/exceptions.py
+++ processqueue/app/exceptions.py
@@ -6,3 +6,8 @@ class NoSuchTaskException(Exception):
 class NoTaskOutputDataException(Exception):
     def __init__(self, task_id: int):
         self.message = f"There is no output data for task with such id = `{task_id}`."
+
+
+class IncorrectTaskOperationException(Exception):
+    def __init__(self, task_id: int):
+        self.message = f"Task id=`{task_id}` has incorrect status for handling this operation."
