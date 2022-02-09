9b6a2270df0e86fb8e8021895667b5f19677cd02
Sergey Stepanov <stepanov.s@numdes.com>
2022-02-09 17:58:52 +0300

    update test_api

--- processqueue/tests/api/test_api.py
+++ processqueue/tests/api/test_api.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 
 import pytest
@@ -18,7 +19,7 @@ async def test_healthcheck(client):
 
 
 @pytest.fixture
-async def first_task_input_data() -> Dict[str, str]:
+def first_task_input_data() -> Dict[str, str]:
     return {
         "input_data": "First Task"
     }
@@ -33,22 +34,38 @@ async def test_create_task(client, first_task_input_data: str):
     assert resp.headers.get("Content-Type") == "application/json"
 
 
-async def test_create_and_run_task(client, first_task_input_data: str):
+async def test_create_and_run_task(client, first_task_input_data):
     resp = await client.post(
-        '/tasks/create',
+        "/tasks/create",
         json=json.dumps(first_task_input_data)
     )
     assert resp.status == 201
     assert resp.headers.get("Content-Type") == "application/json"
-
     data = await resp.json()
+
     task_id = data["task_id"]
+
     run_task_data = {
         "task_id": task_id
     }
     resp = await client.post(
-        '/tasks/run',
-        json=json.dumps(run_task_data)
+        "/tasks/run",
+        json=run_task_data
     )
-    assert resp.status == 201
+    assert resp.status == 200
     assert resp.headers.get("Content-Type") == "application/json"
+
+    while True:
+        resp = await client.get(
+            f"/tasks/{task_id}/status"
+        )
+        data = await resp.json()
+        if data["status"] == "SUCCESS" or data["status"] == "FAILURE":
+            break
+        await asyncio.sleep(0.5)
+
+    resp = await client.get(
+        f"/tasks/{task_id}/output"
+    )
+    data = await resp.json()
+    assert data["output_data"] == f"{first_task_input_data['input_data']} - successfully executed"
